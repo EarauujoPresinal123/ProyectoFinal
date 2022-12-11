a05ee6e4ec108e1ccb277fab8c95fb31a6b951d9
Estefany Araujo <Estefany Araujo@DESKTOP-CKQHEKU>
2022-12-10 22:14:05 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoFinal/clsCalculadora.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal
+{
+    public class clsCalculadora
+    {
+
+        public  int Suma(int a, int b)
+        {
+
+            return a + b;
+        }
+        public static int Resta(int a, int b)
+        {
+
+            return a - b;
+        }
+
+        public static int Multiplicacion(int a, int b)
+        {
+
+            return a * b;
+        }
+
+        public static double Division(int a, int b)
+        {
+
+            return a / b;
+        }
+    }
+}
---
+++ Test/UnitTest1.cs
@@ -0,0 +1,66 @@
+using ProyectoFinal;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [DataRow(4, 4, 8)]
+        ////[DataRow(4, 0, -1)]
+        ////[DataRow(20, 5, 4)]
+        [TestMethod]
+        public void Suma_Retonar_Valor(int a, int b, int esperado)
+        {
+            //Arrange
+            //int a = 4;
+            //int b = 4;
+            //int esperado = 8;
+
+            //Act
+            int actual = clsCalculadora.Suma(a, b);
+            Assert.AreEqual(esperado, actual);
+
+        }
+
+        [DataRow(4, 4, 0)]
+        [TestMethod]
+        public void Resta_Retonar_Valor(int a, int b, int esperado)
+        {
+            //Arrange
+            //int a = 4;
+            //int b = 4;
+            //int esperado = 0;
+            //Act
+            int actual = clsCalculadora.Resta(a, b);
+            Assert.AreEqual(esperado, actual);
+        }
+
+        [DataRow(4, 4, 16)]
+        [TestMethod]
+        public void Multiplicacion_Retonar_Valor(int a, int b, int esperado)
+        {
+            //Arrange
+            //int a = 4;
+            //int b = 4;
+            //int esperado = 16;
+            //Act
+            int actual = clsCalculadora.Multiplicacion(a, b);
+            Assert.AreEqual(esperado, actual);
+        }
+
+        [DataRow(4, 4, 1)]
+        [TestMethod]
+        public void Division_Retonar_Valor(int a, int b, int esperado)
+        {
+            //Arrange
+            //int a = 4;
+            //int b = 4;
+            //int esperado = 0;
+            //Act
+            double actual = clsCalculadora.Division(a, b);
+            Assert.AreEqual(esperado, actual);
+        }
+
+
+    }
+}
